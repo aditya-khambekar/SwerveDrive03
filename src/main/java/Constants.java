ccb53870a252e0b5179ac51fe1e6f8426cceb653
Aditya <adityakhambekar9@gmail.com>
2024-10-25 22:52:34 -0500

    Constants

---
+++ src/main/java/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static final int WHEEL1 = 1;
+    public static final int CONTROLLER1 = 2;
+    public static final int ENCODER1 = 9;
+    public static final int WHEEL2 = 3;
+    public static final int CONTROLLER2 = 4;
+    public static final int ENCODER2 = 10;
+    public static final int WHEEL3 = 5;
+    public static final int CONTROLLER3 = 6;
+    public static final int ENCODER3 = 11;
+    public static final int WHEEL4 = 7;
+    public static final int CONTROLLER4 = 8;
+    public static final int ENCODER4 = 12;
+}
